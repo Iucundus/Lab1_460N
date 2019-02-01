92ae2f2ae307dbca194e3fc5db366058d2b44fbd
John Koelling <john.koelling@utexas.edu>
2019-02-01 09:56:32 -0600

    Copied starter code into assembler.c
    Open/closing files and readAndParse()

--- assembler.c
+++ assembler.c
@@ -1,11 +1,109 @@
 /*
+	Name 1: John Koelling
+	Name 2: Grayson Watkins
+	UTEID 1: JKK887
+	UTEID 2: UTEID of the second partner
+*/
+/*
 EE 460N, Lab 1
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!");
-	return EXIT_SUCCESS;
+/*
+Parsing Assembly Language
+Take a line of the input file and parse it into corresponding fields. Note that you need to write the isOpcode(char*) function which determines whether a string of characters is a valid opcode.
+*/
+#define MAX_LINE_LENGTH 255
+enum
+{
+   DONE, OK, EMPTY_LINE
+};
+
+int	readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4	) {
+   char * lRet, * lPtr;
+   int i;
+   if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
+	return( DONE );
+   for( i = 0; i < strlen( pLine ); i++ )
+	pLine[i] = tolower( pLine[i] );
+
+   /* convert entire line to lowercase */
+   *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+
+   /* ignore the comments */
+   lPtr = pLine;
+
+   while( *lPtr != ';' && *lPtr != '\0' &&
+   *lPtr != '\n' )
+	lPtr++;
+
+   *lPtr = '\0';
+   if( !(lPtr = strtok( pLine, "\t\n ," ) ) )
+	return( EMPTY_LINE );
+
+   if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
+   {
+	*pLabel = lPtr;
+	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+   }
+
+   *pOpcode = lPtr;
+   if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	*pArg1 = lPtr;
+
+	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	*pArg2 = lPtr;
+	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	*pArg3 = lPtr;
+	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	*pArg4 = lPtr;
+	return( OK );
+}
+
+
+FILE* infile = NULL;
+FILE* outfile = NULL;
+
+int main(int argc, char* argv[]) {
+
+     /* open the source file */
+     infile = fopen(argv[1], "r");
+     outfile = fopen(argv[2], "w");
+
+     if (!infile) {
+       printf("Error: Cannot open file %s\n", argv[1]);
+       exit(4);
+		 }
+     if (!outfile) {
+       printf("Error: Cannot open file %s\n", argv[2]);
+       exit(4);
+     }
+
+     /* Do stuff with files */
+	char lLine[MAX_LINE_LENGTH + 1],
+		*lLabel, *lOpcode, *lArg1, *lArg2, *lArg3, *lArg4;
+	int lRet;
+	FILE * lInfile;
+
+	lInfile = fopen( "data.in", "r" );	// open the input file
+	while( lRet != DONE ) {
+		lRet = readAndParse( lInfile, lLine, &lLabel,
+			&lOpcode, &lArg1, &lArg2, &lArg3, &lArg4 );
+		if( lRet != DONE && lRet != EMPTY_LINE )
+		{
+			printf("%s %s %s %s %s %s\n", lLabel, lOpcode, lArg1, lArg2, lArg3, lArg4);
+		}
+	};
+
+
+     /* Close the files*/
+
+     fclose(infile);
+     fclose(outfile);
 }
