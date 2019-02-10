1f50ab647b0e3ec078a0b958f1e7a6fdf7a44377
John Koelling <john.koelling@utexas.edu>
2019-02-09 19:10:14 -0600

    Tweaked shuffle.asm
    Used two shift instructions to avoid overflow

--- shuffle.asm
+++ shuffle.asm
@@ -4,9 +4,11 @@
 
 START   LDW R2, R1, #0 ; R2 contains data from Arr[i]
         LDW R3, R1, #0 ; R3 also contains data from Arr[i]
-        RSHFL R3, R3, #16 ; leftmost bits shifted to right
-        LSHF R2, R2, #16 ; Rightmost bits shifted to left
-        ADD R2, R2, R3  ; this should work right?
+        RSHFL R3, R3, #8 ; leftmost bits shifted to right
+	RSHFL R3, R3, #8
+        LSHF R2, R2, #8 ; Rightmost bits shifted to left
+	LSHF R2, R2, #8 
+        ADD R2, R2, R3
         STW R2, R1, #0 ; store result back in Arr[i]
         ADD R1, R1, #2 ; increment address
         ADD R0, R0, #-1 ; decrement array counter (i)
